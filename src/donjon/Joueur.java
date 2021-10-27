7c570aeb554892d9d7bac953a58107f1b66146e6
walid <wldblm@icloud.com>
2021-10-27 08:18:25 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/donjon/Joueur.java
+++ src/donjon/Joueur.java
@@ -18,6 +18,7 @@ public class Joueur extends Personnage {
 		
 	}
 	public String deplacer(String donjon[][]) {
+		
 		System.out.println("Dans quel direction souhaitez vous aller ?");
 		System.out.println("1 = haut");
 		System.out.println("2 = droite");
